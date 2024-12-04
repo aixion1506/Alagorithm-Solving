3a01774df216b6a90bcbf9b57dd39ff94027d22d
aixion1506 <aixion1506@gmail.com>
2024-12-04 17:32:42 +0900

    첫번째 문제 원인 분석

--- A+B.js
+++ A+B.js
@@ -1,21 +1,25 @@
-const readline = require("readline");
+/** 시간초과로 실패 **/
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-rl.on("line", (line) => {
-  const numbers = line.split(" ").map(Number);
+// rl.on("line", (line) => {
+//   const numbers = line.split(" ").map(Number);
 
-  let sum = 0;
+//   let sum = 0;
 
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log("Sum:", sum);
-});
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log("Sum:", sum);
+// });
 
-rl.on("close", () => {});
+// rl.on("close", () => {});
 
-// 시간초과로 실패
+/**
+ * 원인 분석
+ * - 백준에서 계속 input을 주지만 제대로 close가 원활히 이루어 지지 않아서 시간 초과 된걸로 예상
+ */
