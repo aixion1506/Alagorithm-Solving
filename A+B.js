196e7b09abe246912fe59c5b8c64ab42906af79f
aixion1506 <aixion1506@gmail.com>
2024-12-04 17:26:18 +0900

    solve: 사용자 입력으로부터 한 줄씩 숫자를 더하는 기능 구현

---
+++ A+B.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  const numbers = line.split(" ").map(Number);
+
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log("Sum:", sum);
+});
+
+rl.on("close", () => {});
