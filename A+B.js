646d1acd1b828f24ac1a1c44069cbb5f0b4ea433
aixion1506 <aixion1506@gmail.com>
2024-12-04 17:37:04 +0900

    2nd: 기존 line의 로직을 close로 이동

--- A+B.js
+++ A+B.js
@@ -23,3 +23,31 @@
  * 원인 분석
  * - 백준에서 계속 input을 주지만 제대로 close가 원활히 이루어 지지 않아서 시간 초과 된걸로 예상
  */
+
+/** 두번째 도전 */
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let inputLines = [];
+
+rl.on("line", (line) => {
+  inputLines.push(line);
+});
+
+rl.on("close", () => {
+  inputLines.forEach((line) => {
+    const numbers = line.split(" ").map(Number);
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    console.log(sum);
+  });
+});
+
+/**
+ * 원인분석
+ * - input을 line단위로 던져서 Close때 로직을 돌리는 식으로 하려 했는데 시간 초과가 뜸..
+ * - 일단 테스트 코드 도입해봐야겠다...
+ */
